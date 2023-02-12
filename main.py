0e19f0a766da9e48164152f51a9edab498619832
Krumlov <62485575+Stakepy@users.noreply.github.com>
2023-02-12 22:58:51 +0100

    v

---
+++ main.py
@@ -0,0 +1,25 @@
+import telebot
+
+# Сюда пишем токен созданный BotFather
+token = ''
+# Создаем экземпляр бота
+bot = telebot.TeleBot(token)
+# Получаем айди чата по его имени
+GROUP_ID = 1001334748695
+# Список матов которые нужно удалять
+blacklist = ['']
+
+# Перехват чужих сообщений в чате
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    # По очереди ищем маты из списка в сообщении
+    for x in blacklist:
+        if (x in message.text):
+            # Если найдены маты удаляем сообщение с ними
+            bot.delete_message(message.chat.id, message.message_id)
+        else:
+            pass
+
+
+    if name == "main":
+        bot.infinity_polling()
