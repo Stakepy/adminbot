5d6ebd633643b401bf6ce61c2f697a7249a882ee
Krumlov <62485575+Stakepy@users.noreply.github.com>
2023-03-29 19:42:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ token = ''
 bot = telebot.TeleBot(token)
 # Получаем айди чата по его имени
 GROUP_ID = 0000000000000
-# Список матов которые нужно удалять
+# Список запрещённых слов которые нужно удалять
 blacklist = ['']
 
 # Перехват чужих сообщений в чате
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    # По очереди ищем маты из списка в сообщении
+    # По очереди ищем запрещённые слова из списка в сообщении
     for x in blacklist:
         if (x in message.text):
-            # Если найдены маты удаляем сообщение с ними
+            # Если найдены запрещённые слова удаляем сообщение с ними
             bot.delete_message(message.chat.id, message.message_id)
         else:
             pass
